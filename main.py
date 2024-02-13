7dca7efbfafa53df1cf05abd799895358f38dea0
Gugh Patrik <patrik.gugh@materna.group>
2024-02-13 09:13:51 +0100

    Save the working solution

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
-data = []
+file = open('data.txt', 'r')
 
-data_file = open('data.txt', 'r')
-num_rows = int(data_file.readline().strip())
-num_cols = int(data_file.readline().strip())
+matrix_rows = int(file.readline())
+matrix_columns = int(file.readline())
 
-for i in range(0, num_rows):
-    line = data_file.readline()
-    line_split = line.split()
-    num_list = list(map(lambda st: int(st), line_split))
-    data.append(num_list)
-data_file.close()
+lines = file.readlines()
+matrix = [[{'num': int(num), 'sel': False} for num in line.split()] for line in lines]
+file.close()
 
-data = data[:5]
+test_matrix = [[-2, 1, 3], [1, -2, 5], [1, -1, 6]]
+ITERS = 1
+while ITERS < 1_000_000:
+    SUM = 0
 
-SUM = 0
+    for i in range(0, len(matrix)):
+        row = matrix[i]
 
-# data = [[-2, 1, 3], [1, -2, 5], [1, -1, 6]]
-results = []
+        # select not-selected numbers
+        not_sel_nums = [n for n in row if not n['sel']]
 
-# iterate through first row
-for i in range(0, len(data[0])):
-    f = data[0][i]
-    results.append([f])
+        if len(not_sel_nums) == 0:
+            continue
 
-    for j in range(1, len(data)):
-        row = data[j]
+        min_number = min(not_sel_nums, key=lambda x: abs((x['num'] + SUM)))
+        min_number['sel'] = True
 
-        s = f + row[0]
-        ni = row[0]
+        SUM += min_number['num']
 
-        for k in range(1, len(row)):
-            n = row[k]
+    if SUM == 0:
+        break
 
-            if abs(f + n) < abs(s):
-                s = f + n
-                ni = n
+    ITERS += 1
 
-        results[i].append(ni)
-        f = s
-
-    print(f'f[{i}] = {f}')
-print(results)
+print(f'iters: {ITERS}')
